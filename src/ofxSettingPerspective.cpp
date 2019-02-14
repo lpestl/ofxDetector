db168c1f09cb945c07e0c6806afb49cef8bf9f33
Mikhail S. Kataev <lpestlname@gmail.com>
2019-02-15 00:18:19 +0300

    Laziness eats me up

--- src/ofxSettingPerspective.cpp
+++ src/ofxSettingPerspective.cpp
@@ -1,2 +1,38 @@
 #include "ofxSettingPerspective.h"
 
+void ofxSettingPerspective::setup(unsigned width_image, unsigned height_image)
+{
+}
+
+void ofxSettingPerspective::update(ofxCvColorImage color_frame)
+{
+}
+
+void ofxSettingPerspective::draw(ofRectangle setting_rect, ofRectangle output_rect)
+{
+}
+
+void ofxSettingPerspective::setMode(mode mode)
+{
+}
+
+ofxSettingPerspective::mode ofxSettingPerspective::getMode() const
+{
+	return mode_;
+}
+
+void ofxSettingPerspective::mouseDragged(int x, int y, int button)
+{
+}
+
+void ofxSettingPerspective::mousePressed(int x, int y, int button)
+{
+}
+
+void ofxSettingPerspective::mouseReleased(int x, int y, int button)
+{
+}
+
+void ofxSettingPerspective::drawCorner(ofRectangle setting_rect, ofPoint& corner)
+{
+}
--- src/ofxSettingPerspective.h
+++ src/ofxSettingPerspective.h
@@ -6,18 +6,27 @@
 class ofxSettingPerspective
 {
 public:
+
+	enum mode { editting, view };
+
 	void setup(unsigned width_image, unsigned height_image);
 	void update(ofxCvColorImage color_frame);
 	void draw(ofRectangle setting_rect, ofRectangle output_rect);
 
-private:
-	void drawCorner(ofRectangle setting_rect, ofPoint & corner);
+	void setMode(mode mode);
+	mode getMode() const;
+
 	void mouseDragged(int x, int y, int button);
 	void mousePressed(int x, int y, int button);
 	void mouseReleased(int x, int y, int button);
 
+private:
+	void drawCorner(ofRectangle setting_rect, ofPoint & corner);
+
+	ofxCvGrayscaleImage		source_;
 	ofxCvGrayscaleImage		result_;
 	std::vector<ofPoint>	setting_corners_;
 	ofPoint					last_mouse_position_;
+	mode					mode_;
 };
 
